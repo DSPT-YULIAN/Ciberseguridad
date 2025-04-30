4f8c585cc64480be9d1497446956c875cb091807
DSPT-YULIAN <yulian-15@hotmail.com>
2025-04-30 09:10:39 -0500

    Notas

---
+++ Lista de herramientas personales.py
@@ -0,0 +1,40 @@
+Caja de herramientas Personal
+
+
+
+Pruebas de penetracion
+
+	
+	Fase 2 Activo Reconocimiento activo /Enumeracion de informacion 
+
+
+		1. enum4linux  // enum4linux -U [host]
+		
+				-U busca usuarios configurados
+				-S obtiene una lista de archivos compartidos
+				-G obtiene una lista de los grupos y sus miembros
+				-P enumera las políticas de contraseñas
+				-i obtiene una lista de impresoras
+				-a Combina las opciones -U, -S, -G, -P, -r, -o, -n, -i en un solo comando
+		
+******************************************************************************
+
+	Fase 3 Explotacion 	
+	
+
+		2.Searchsploit
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- README.md
+++ README.md
@@ -9,3 +9,20 @@ Fase de reconomiento con Nmap
 2. Identifique los puertos y servicios abiertos. nmap -sV (host)
 3. Identificar el sistema operativo  sudo nmap -O (host) OS details
 4. Utilice el guion de Nmap Vulners para buscar vulnerabilidades. nmap -sV --script vulners --script-args mincvss=4 (host)
+
+
+
+root@kali:/usr/share/nmap/scripts# ls -1 snmp*
+snmp-brute.nse
+snmp-hh3c-logins.nse
+snmp-info.nse
+snmp-interfaces.nse
+snmp-ios-config.nse
+snmp-netstat.nse
+snmp-processes.nse
+snmp-sysdescr.nse
+snmp-win32-services.nse
+snmp-win32-shares.nse
+snmp-win32-software.nse
+snmp-win32-users.nse
+root@kali:/usr/share/nmap/scripts#
